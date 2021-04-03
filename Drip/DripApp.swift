3b1eec77a0e1d086d2cf42d62cab7d880283e262
Sooni-Code1234 <sooni.mohammed@gmail.com>
2021-04-03 15:48:48 -0400

    Initial Commit

---
+++ Drip/DripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DripApp.swift
+//  Drip
+//
+//  Created by Sooni Mohammed on 2021-04-03.
+//
+
+import SwiftUI
+
+@main
+struct DripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
